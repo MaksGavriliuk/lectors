c606cce76bdbbf008b99bfcb6517e71538e58308
MaksGavriliuk <maks.revival27@gmail.com>
2024-03-24 21:55:46 +0300

    change: cascade remove degrees -> lectors

--- src/main/java/org/example/lectors/entities/Degree.java
+++ src/main/java/org/example/lectors/entities/Degree.java
@@ -1,17 +1,23 @@
 package org.example.lectors.entities;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.Accessors;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
@@ -29,5 +35,9 @@ public class Degree {
 
     @Column(name = "degree", nullable = false)
     private String name;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "degree", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Lector> lectors = new ArrayList<>();
 
 }
